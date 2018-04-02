e414244998c9e4f584a557e253727c97f978ae41
greenux <greenux@naver.com>
2018-04-01 23:55:29 -0700

    activate A20_gate and check memory area were added

--- 01.Kernel32/Source/EntryPoint.s
+++ 01.Kernel32/Source/EntryPoint.s
@@ -9,7 +9,21 @@ START:
 	mov ax, 0x1000
 	mov ds, ax
 	mov es, ax
+;
+;activate a20 gate with bios service iferror occour go to system control port
+;
+	mov ax, 0x2401
+	int 0x15
 
+	jc .A20GATEERROR
+	jmp .A20GATESUCCESS
+
+.A20GATEERROR:
+		in al, 0x92
+		or al, 0x02
+		and al, 0xfe
+		out 0x92,al
+.A20GATESUCCESS:
 	cli
 	lgdt [GDTR]
 	mov eax, 0x4000003B
--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -1,10 +1,34 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char *pcString);
-
+BOOL kInitializeKernel64Area(void);
+BOOL kIsMemoryEnough(void);
 void main(void)
 {
+	DWORD i;
 	kPrintString(0,3,"C Language Kernel Started~!!!");
+
+	//check minimum memory size
+	kPrintString(0,4,"Minimum memory size check ......[    ]");
+	if(kIsMemoryEnough()==FALSE)
+	{
+		kPrintString(33,4,"FAIL");
+		kPrintString(0,5,"Not enough memory ~~FMOS Requires over 64MByte memory");
+		while(1);
+	}
+	else
+	{
+		kPrintString(33,4,"PASS");
+	}
+	//initialize ia-32e mode kernel area
+	kPrintString(0,5,"ia-32ekernel area initialize....[    ]");
+	if(kInitializeKernel64Area()==FALSE)
+	{
+		kPrintString(33,5,"FAIL");
+		kPrintString(0,6,"kernel Area initialization fail");
+		while(1);
+	}
+	kPrintString(33,5,"PASS");
 
 	while(1);
 }
@@ -18,4 +42,38 @@ void kPrintString(int iX, int iY,const char *pcString)
 	{
 		pstScreen[i].bCharactor=pcString[i];
 	}
+}
+BOOL kInitializeKernel64Area(void)
+{
+	DWORD *pdwCurrentAddress;
+
+	//initialize allocated address 0x10000
+	pdwCurrentAddress=(DWORD *)0x100000;
+	while((DWORD)pdwCurrentAddress< 0x600000)
+	{
+		*pdwCurrentAddress=0x00;
+		if(*pdwCurrentAddress!=0)
+		{
+			return FALSE;
+		}
+		pdwCurrentAddress++;
+	}
+	return TRUE;
+}
+BOOL kIsMemoryEnough(void)
+{
+	DWORD *pdwCurrentAddress;
+	//starting point 0x100000(1MB)
+	pdwCurrentAddress=(DWORD *)0x100000;
+	//upto 64MB
+	while((DWORD)pdwCurrentAddress<0x400000)
+	{
+		*pdwCurrentAddress=0x12345678;
+		if(*pdwCurrentAddress!=0x12345678)
+		{
+			return FALSE;
+		}
+		pdwCurrentAddress+=(0x100000/4);
+	}
+	return TRUE;
 }
